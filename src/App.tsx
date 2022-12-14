2475f6ad7d310f02b747a8cd857cb2c22a671a89
dmitrysmaltsev <dmitry.s.maltsev@gmail.com>
2022-12-14 23:43:02 +0000

    Using HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import { Today } from "./components/Today";
 import { Tomorrow } from "./components/Tomorrow";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Today />} />
         <Route path="/tomorrow" element={<Tomorrow />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
